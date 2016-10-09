8b7c1617b0c4900ad2e5fe2756eefb0d3cd8be24
caiqiqi <564933437@qq.com>
2016-10-10 01:07:25 +0800

    加入 判断操作系统调整字体.py & git_log_color.sh

---
+++ git_log_color.sh
@@ -0,0 +1,2 @@
+# 可以让git的log信息更漂亮地显示出来
+git config --global alias.lg "log --color --graph --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(bold white)— %an%C(reset)%C(bold yellow)%d%C(reset)' --abbrev-commit --date=relative"
---
+++ 判断操作系统调整字体.py
@@ -0,0 +1,7 @@
+#coding=utf-8
+import platform
+
+if platform.system() == "Windows":
+    SHELL_FONT = "Lucida Console 9"
+else:
+    SHELL_FONT = "Luxi Mono 10"
